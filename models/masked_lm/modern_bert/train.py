f8eeb580af6c75cb8be47b0ba85f1fd622ab4028
BounharAbdelaziz <bounhar.abdelaziz@gmail.com>
2025-02-20 12:31:11 +0100

    new config params

--- models/masked_lm/modern_bert/train.py
+++ models/masked_lm/modern_bert/train.py
@@ -17,94 +17,53 @@ from utils import (
     pre_processing
 )
 
-if __name__ == "__main__":
-    
-    DATASET_NAME="atlasia/AL-Atlas-Moroccan-Darija-Pretraining-Dataset"
-    
-    info("Loading dataset...")
-    dataset=load_dataset(DATASET_NAME)
-    info("Dataset loaded.")
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-    configs=Config()
-    info("Loading base tokenizer...")
-    base_tokenizer=AutoTokenizer.from_pretrained(
-        "answerdotai/ModernBERT-base",
-        use_fast=True # Fast tokenizers are implemented in Rust and are significantly faster than the regular Python-based tokenizers.
-    )
-
-    # check if the tokenizer is not already trained
-    if os.path.exists(f"{configs.base_dir}/tokenizer"):
-        info("Loading the pretrained new tokenizer...")
-        new_tokenizer=AutoTokenizer.from_pretrained(f"{configs.base_dir}/tokenizer",use_fast=True)
-        info("New tokenizer loaded.")
-        
-    else:
-        info("Training new Darija tokenizer")
-        train_iterator=batch_iter(dataset["train"])
-        new_tokenizer=base_tokenizer.train_new_from_iterator(
-            text_iterator=train_iterator,
-            vocab_size=configs.new_vocab_size,
-            show_progress=True
-        )
-
-        info("Saving the new tokenizer...")
-        new_tokenizer.save_pretrained(f"{configs.base_dir}/tokenizer")
-        info("New tokenizer saved.")
-
-        info("Load the new Darija tokenizer")
-        new_tokenizer = AutoTokenizer.from_pretrained(f"{configs.base_dir}/tokenizer",use_fast=True)
-
-    info("Tokenizing train/test datasets...")
-    train_dataset=dataset["train"].map(
-        lambda example: pre_processing(example,new_tokenizer,configs),
-        batched=True,
-        remove_columns=dataset["train"].column_names
-    )
-    test_dataset=dataset["test"].map(
-        lambda example: pre_processing(example,new_tokenizer,configs),
-        batched=True,
-        remove_columns=dataset["test"].column_names
-    )
-    
-    # info("Counting total tokens in training dataset...")
-    # total_tokens = sum(len(new_tokenizer(example["text"]).input_ids) for example in dataset["train"])
-    # info(f"Total tokens in training dataset: {total_tokens}")
-
-    info("Initializing data collator...")
-    data_collator=DataCollatorForLanguageModeling(
-        tokenizer=new_tokenizer,
-        mlm=True,
-        mlm_probability=configs.mlm_probability
-    )
-
-    info("Loading base model...")
-    import os
-import torch
-from datasets import load_dataset
-from transformers import (
-    AutoTokenizer,
-    AutoModelForMaskedLM,
-    AutoConfig,
-    DataCollatorForLanguageModeling,
-    TrainingArguments,
-    Trainer
-)
-
-from utils import (
-    Config,
-    info,
-    batch_iter,
-    pre_processing
-)
-
 import wandb
 
 if __name__ == "__main__":
-    configs=Config()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
+    configs=Config(
+        data_path="ClusterlabAi/101_billion_arabic_words_dataset", #"wikimedia/wikipedia", #"atlasia/AL-Atlas-Moroccan-Darija-Pretraining-Dataset",
+        hub_path="BounharAbdelaziz/Modern-BERT-Arabic-Base", #"BounharAbdelaziz/Modern-BERT-Morocco-Darija-Base",
+        base_dir="./modernbert_ar_101B_base",
+        max_length=1024,
+        new_vocab_size=70_000,
+        num_train_epochs=1,
+        batch_size=16,
+        gradient_accumulation_steps=16,
+        max_grad_norm=1,
+        lr=5e-4,
+        warmup_ratio=0.05,
+        version="v1",
+        logging_steps=50,
+        eval_steps=100,
+        save_steps=100,        
+    )
+    ARABIC_TRAIN = True
+    
+    # configs=Config(
+    #     data_path="atlasia/AL-Atlas-Moroccan-Darija-Pretraining-Dataset",
+    #     hub_path="BounharAbdelaziz/Modern-BERT-Morocco-Darija-Base",
+    #     base_dir="./modernbert_darija_base",
+    #     num_train_epochs=2,
+    #     batch_size=8,
+    #     gradient_accumulation_steps=16,
+    #     max_grad_norm=1,
+    #     lr=5e-3,
+    #     warmup_ratio=0.07,
+    #     version="v1",
+    #     logging_steps=50,
+    #     eval_steps=100,
+    #     save_steps=100,        
+    # )
+    # ARABIC_TRAIN = False
+    
+    arabic_config = "20231101.ar"
     DATASET_NAME = configs.data_path
+    print(f'DATASET_NAME: {DATASET_NAME}')
+    print(f'ARABIC_TRAIN: {ARABIC_TRAIN}')
+    print(f'arabic_config: {arabic_config}')
+    print(f'hub_path: {configs.hub_path}')
     
     # Initialize wandb
     wandb.init(
@@ -117,7 +76,10 @@ if __name__ == "__main__":
     )
     
     info("Loading dataset...")
-    dataset=load_dataset(DATASET_NAME)
+    if ARABIC_TRAIN:
+        dataset=load_dataset(DATASET_NAME) #, arabic_config)
+    else:
+        dataset=load_dataset(DATASET_NAME)
     info("Dataset loaded.")
 
     info("Loading base tokenizer...")
@@ -153,11 +115,12 @@ if __name__ == "__main__":
         batched=True,
         remove_columns=dataset["train"].column_names
     )
-    test_dataset=dataset["test"].map(
-        lambda example: pre_processing(example,new_tokenizer,configs),
-        batched=True,
-        remove_columns=dataset["test"].column_names
-    )
+    if not ARABIC_TRAIN:
+        test_dataset=dataset["test"].map(
+            lambda example: pre_processing(example,new_tokenizer,configs),
+            batched=True,
+            remove_columns=dataset["test"].column_names
+        )
 
     # info("Counting total tokens in training dataset...")
     # total_tokens = sum(len(new_tokenizer(example["text"]).input_ids) for example in dataset["train"])
@@ -189,8 +152,8 @@ if __name__ == "__main__":
         num_train_epochs=configs.num_train_epochs,
         per_device_train_batch_size=configs.per_device_train_batch_size,
         per_device_eval_batch_size=configs.per_device_eval_batch_size,
-        evaluation_strategy=configs.evaluation_strategy,
-        eval_steps=configs.eval_steps,
+        evaluation_strategy=configs.evaluation_strategy if not ARABIC_TRAIN else "no",
+        eval_steps=configs.eval_steps if not ARABIC_TRAIN else None,
         logging_steps=configs.logging_steps,
         save_steps=configs.save_steps,
         save_total_limit=configs.save_total_limit,
@@ -200,6 +163,7 @@ if __name__ == "__main__":
         report_to=configs.report_to,
         run_name=configs.run_name,
         gradient_accumulation_steps=configs.gradient_accumulation_steps,
+        lr_scheduler_type="constant_with_warmup", # mimic the trapezoidal schedule, we do decay at the end
     )
     info("Done!")
 
@@ -208,7 +172,7 @@ if __name__ == "__main__":
         model=model,
         args=training_args,
         train_dataset=train_dataset,
-        eval_dataset=test_dataset,
+        eval_dataset=test_dataset if not ARABIC_TRAIN else None,
         data_collator=data_collator
     )
     info("Done!")
--- models/masked_lm/modern_bert/utils.py
+++ models/masked_lm/modern_bert/utils.py
@@ -1,7 +1,12 @@
 class Config:
   def __init__(
     self,
-    num_train_epochs=2,
+    data_path="atlasia/AL-Atlas-Moroccan-Darija-Pretraining-Dataset",
+    hub_path="BounharAbdelaziz/Modern-BERT-Morocco-Darija-Base",
+    base_dir="./modernbert_base",
+    max_length=8192,
+    new_vocab_size=70_000,
+    num_train_epochs=3,
     batch_size=8,
     gradient_accumulation_steps=16,
     max_grad_norm=1,
@@ -13,13 +18,19 @@ class Config:
     save_steps=100,
   ):
     self.base_model_name="answerdotai/ModernBERT-base"
-    self.data_path="atlasia/AL-Atlas-Moroccan-Darija-Pretraining-Dataset"
-    self.hub_path="BounharAbdelaziz/Modern-BERT-Morocco-Darija-Base"
-    self.max_length=8192
-    self.new_vocab_size=64000
+    self.data_path=data_path
+    self.hub_path=hub_path
+    self.max_length=max_length
+    self.new_vocab_size=new_vocab_size
     self.mlm_probability=0.15
-    self.base_dir="./modernbert_base"
-    self.output_dir=self.base_dir+"/model"
+    
+    # build and check run name    
+    self.run_name = f'{self.base_model_name.split("/")[-1]}-bs-{batch_size}-lr-{lr}-ep-{num_train_epochs}-wp-{warmup_ratio}-gacc-{gradient_accumulation_steps}-gnm-{max_grad_norm}-{version}'
+    assert '--' not in self.run_name, f"[WARN] Detected -- in run_name. This will cause a push_to_hub error! Found run_name={self.run_name} "
+    assert len(self.run_name) < 96, f"[WARN] run_name too long, found len(run_name)={len(self.run_name)} > 96. This will cause a push_to_hub error! Consider squeezing it. Found run_name={self.run_name}"
+    
+    self.base_dir=base_dir
+    self.output_dir=self.base_dir+f"/{self.run_name}"
     self.num_train_epochs=num_train_epochs
     self.per_device_train_batch_size=batch_size
     self.per_device_eval_batch_size=batch_size
@@ -31,14 +42,10 @@ class Config:
     self.save_total_limit=1
     self.learning_rate=lr
     self.warmup_ratio=warmup_ratio
-    self.weight_decay=0.01
+    self.weight_decay=1e-5
     self.report_to="wandb"
     self.run_name="al_atlas_masked_lm"
-    # build and check run name    
-    self.run_name = f'{self.base_model_name.split("/")[-1]}-bs-{batch_size}-lr-{lr}-ep-{num_train_epochs}-wp-{warmup_ratio}-gacc-{gradient_accumulation_steps}-gnm-{max_grad_norm}-{version}'
-    assert '--' not in self.run_name, f"[WARN] Detected -- in run_name. This will cause a push_to_hub error! Found run_name={self.run_name} "
-    assert len(self.run_name) < 96, f"[WARN] run_name too long, found len(run_name)={len(self.run_name)} > 96. This will cause a push_to_hub error! Consider squeezing it. Found run_name={self.run_name}"
-    
+   
     self.wandb_project_name="al_atlas_masked_lm"
     self.overwrite_output_dir = True
     
